22fad4c4b7cf552c95b117f3f9435c3b49d9d902
charankanchipati <150544602+charankanchipati@users.noreply.github.com>
2024-04-28 10:32:25 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,11 @@
+let payment=document.getElementById("now");
+let pay=document.getElementById("can");
+function scanner(){
+    payment.classList.add("open-scan");
+}
+function close(){
+    payment.classList.add("close-scan");
+}
+function debi(){
+    pay.classList.add("open-scan");
+}
